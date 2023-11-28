9d2ec5c28255b192e3c65fc0da4f9d5c55486849
Miguel Guerra <mivue.fran2@gmail.com>
2023-11-28 08:59:17 -0500

    Producer and onsumer fora Kafka

---
+++ Consumer/Consumer.cs
@@ -0,0 +1,57 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+
+class Consumer
+{
+
+    public static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Please provide the configuration file path as a command line argument");
+        }
+
+        const string topic = "purchases";
+
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "pkc-lzvrd.us-west4.gcp.confluent.cloud:9092",
+            SecurityProtocol = SecurityProtocol.SaslSsl,
+            SaslMechanism = SaslMechanism.Plain,
+            SaslUsername = "R2KYS6HSNT7YCRU7",
+            SaslPassword = "7VI3JJlpmnc7XTginA5+rE6BZfb83qxrz8CHvm/wfEmOJhKUt2InFGMYhH+YQG4F",
+            GroupId = "kafka-dotnet-getting-started",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        CancellationTokenSource cts = new();
+        Console.CancelKeyPress += (_, e) =>
+        {
+            e.Cancel = true; // prevent the process from terminating.
+            cts.Cancel();
+        };
+
+        using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+        {
+            consumer.Subscribe(topic);
+            try
+            {
+                while (true)
+                {
+                    var cr = consumer.Consume(cts.Token);
+                    Console.WriteLine($"Consumed event from topic {topic}: key = {cr.Message.Key,-10} value = {cr.Message.Value}");
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Ctrl-C was pressed.
+            }
+            finally
+            {
+                consumer.Close();
+            }
+        }
+    }
+}
---
+++ Producer/Producer.cs
@@ -0,0 +1,56 @@
+﻿using Confluent.Kafka;
+using System;
+using Microsoft.Extensions.Configuration;
+
+class Producer
+{
+    public static async Task Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Please provide the configuration file path as a command line argument");
+        }
+
+        const string topic = "purchases";
+
+        string[] users = { "eabara", "jsmith", "sgarcia", "jbernard", "htanaka", "awalther" };
+        string[] items = { "book", "alarm clock", "t-shirts", "gift card", "batteries" };
+
+        var config = new ProducerConfig
+        {
+            BootstrapServers = "pkc-lzvrd.us-west4.gcp.confluent.cloud:9092", 
+            SecurityProtocol= SecurityProtocol.SaslSsl,
+            SaslMechanism = SaslMechanism.Plain,
+            SaslUsername = "R2KYS6HSNT7YCRU7",
+            SaslPassword = "7VI3JJlpmnc7XTginA5+rE6BZfb83qxrz8CHvm/wfEmOJhKUt2InFGMYhH+YQG4F",
+        };
+
+        using (var producer = new ProducerBuilder<string, string>(config).Build())
+        {
+            var numProduced = 0;
+            Random rnd = new();
+            const int numMessages = 10;
+            for (int i = 0; i < numMessages; ++i)
+            {
+                var user = users[rnd.Next(users.Length)];
+                var item = items[rnd.Next(items.Length)];
+
+                await producer.ProduceAsync(topic, new Message<string, string> { Key = user, Value = item })
+                    .ContinueWith(task =>
+                    {
+                        if (task.IsFaulted)
+                        {
+                            Console.WriteLine($"Failed to deliver message: {task.Exception.Message}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Produced event to topic {topic}: key = {user,-10} value = {item}");
+                            numProduced += 1;
+                        }
+                    });
+            }
+            producer.Flush(TimeSpan.FromSeconds(10));
+            Console.WriteLine($"{numProduced} messages were produced to topic {topic}");
+        }
+    }
+}
